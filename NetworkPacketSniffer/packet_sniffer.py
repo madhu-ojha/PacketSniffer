a23e6ce411412e83c4669439443eb63189c1a238
mso <madhuojha34@gmail.com>
2024-02-23 23:28:42 +0545

    unpacking ipv4 packet

--- NetworkPacketSniffer/packet_sniffer.py
+++ NetworkPacketSniffer/packet_sniffer.py
@@ -1,4 +1,5 @@
 # works for unix based system only
+# needs sudo privilege
 import socket
 import struct
 # from scapy.all import *
@@ -16,8 +17,17 @@ def main():
         print('Destination: {}, Source: {}, Protocol: {}'.format(
             dest_mac, src_mac, eth_proto))
 
-
+        # for ipv4
+        if eth_proto == 8:
+            (version, header_length, ttl, protocol,
+             src, target, data) = ipv4_pack(org_data)
+            print('\nIPv4 Packet:')
+            print('\tVersion: {}, Header Length: {}, TTL: {}'.format(
+                version, header_length, ttl))
+            print('\tProtocol: {}, Source: {}, Target: {}'.format(
+                protocol, src, target))
 # unpacking ethernet frame
+
 
 def ethernet_unpack(raw_data):
     dest_address, src_address, proto = struct.unpack(
@@ -31,6 +41,22 @@ def get_mac(addr_bytes):
     addr_str = map('{:02x}'.format, addr_bytes)
     formatted_mac = ':'.join(addr_str).upper()
     return formatted_mac
+
+# Unpacking ipv4 packet
+
+
+def ipv4_pack(data):
+    version_length = data[0]
+    version = version_length >> 4
+    header_length = (version_length & 15) * 4
+    ttl, protocol, src, target = struct.unpack('! 8x BB 2x 4s 4s', data[:20])
+    return version, header_length, ttl, protocol, ipv4(src), ipv4(target), data[header_length:]
+
+# return formatted ipv4 address
+
+
+def ipv4(address):
+    return '.'.join(map(str, address))
 
 
 main()
