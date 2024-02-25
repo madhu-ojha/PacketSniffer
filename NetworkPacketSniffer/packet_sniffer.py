0ee4c4c3d5cf30eea25d028b93a35b9e6c9da910
mso <madhuojha34@gmail.com>
2024-02-25 20:54:53 +0545

    unpacking TCP segment

--- NetworkPacketSniffer/packet_sniffer.py
+++ NetworkPacketSniffer/packet_sniffer.py
@@ -58,5 +58,28 @@ def ipv4_pack(data):
 def ipv4(address):
     return '.'.join(map(str, address))
 
+# Unpacking ICMP packet
+
+
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+#unpack TCP segment
+def tcp_segmet(data):
+    (src_port, dest_port, sequence, acknowledgement,offset_reserved_flags) = struct.unpack('! H H L L H'data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin =  offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+    
+
+
+
+
 
 main()
