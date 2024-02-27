f7d8ef5ebf6f28a854380111302a56ce9e6e32ec
mso <madhuojha34@gmail.com>
2024-02-27 17:55:15 +0545

    icmp protocol

--- NetworkPacketSniffer/packet_sniffer.py
+++ NetworkPacketSniffer/packet_sniffer.py
@@ -2,6 +2,7 @@
 # needs sudo privilege
 import socket
 import struct
+import textwrap
 # from scapy.all import *
 
 
@@ -26,8 +27,18 @@ def main():
                 version, header_length, ttl))
             print('\tProtocol: {}, Source: {}, Target: {}'.format(
                 protocol, src, target))
-# unpacking ethernet frame
 
+            # ICMP protocol
+            if protocol == 1:
+                icmp_type, code, checksum, data = icmp_packet(data)
+                print("ICMP Packet: ",)
+                print('Type: {}, Code: {}, CHecksum: {}'.format(
+                    icmp_type, code, checksum))
+                print('Data:')
+                print(format_multi_line('\t\t\t', data))
+
+
+# unpacking ethernet frame
 
 def ethernet_unpack(raw_data):
     dest_address, src_address, proto = struct.unpack(
@@ -65,25 +76,38 @@ def icmp_packet(data):
     icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
     return icmp_type, code, checksum, data[4:]
 
-#unpack TCP segment
+# unpack TCP segment
+
+
 def tcp_segmet(data):
-    (src_port, dest_port, sequence, acknowledgement,offset_reserved_flags) = struct.unpack('! H H L L H'data[:14])
+    (src_port, dest_port, sequence, acknowledgement,
+     offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
     offset = (offset_reserved_flags >> 12) * 4
     flag_urg = (offset_reserved_flags & 32) >> 5
     flag_ack = (offset_reserved_flags & 16) >> 4
     flag_psh = (offset_reserved_flags & 8) >> 3
     flag_rst = (offset_reserved_flags & 4) >> 2
     flag_syn = (offset_reserved_flags & 2) >> 1
-    flag_fin =  offset_reserved_flags & 1
+    flag_fin = offset_reserved_flags & 1
     return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
-    
+
 # unpack udp_segment
+
+
 def udp_segment(data):
     src_port, dest_port, size = struct.unpack('! H H 2x H ', data[:8])
     return src_port, dest_port, size, data[8:]
-    
+
+# Formats multi-line data
 
 
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 
 main()
